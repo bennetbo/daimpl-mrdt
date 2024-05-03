d190ccc806d0f23e264988afef5ffae6fa7db440
Bennet Bo Fenner <53836821+bennetbo@users.noreply.github.com>
2024-05-03 10:56:30 +0200

    cleanup integration test

--- src/test/scala/mrdt/IntegrationTests.scala
+++ src/test/scala/mrdt/IntegrationTests.scala
@@ -5,34 +5,33 @@ import org.scalatest.funsuite.AnyFunSuite
 class IntegrationTests extends AnyFunSuite {
   class TodoItem(val id: Int, val text: String) extends Ordered[TodoItem] {
     override def compare(that: TodoItem): Int = this.id - that.id
+    override def equals(x: Any): Boolean = x match {
+      case that: TodoItem => this.id == that.id
+      case _              => false
+    }
+    override def hashCode(): Int = this.id
   }
 
   test("todo_list") {
-    val buyMilk = new TodoItem(1, "Buy milk")
-    val walkDog = new TodoItem(2, "Walk the dog")
-    val doLaundry = new TodoItem(3, "Do laundry")
-    val cleanHouse = new TodoItem(4, "Clean the house")
-    val cookDinner = new TodoItem(5, "Cook dinner")
-
     val state = List.of(
-      buyMilk,
-      walkDog,
-      doLaundry
+      TodoItem(1, "Buy milk"),
+      TodoItem(2, "Walk the dog"),
+      TodoItem(3, "Do laundry")
     )
 
     val replica1 = state.clone()
-    replica1.insert(0, cleanHouse)
+    replica1.insert(0, TodoItem(4, "Clean the house"))
 
     val replica2 = state.clone()
     replica2.removeAt(1)
-    replica2.insert(0, cookDinner)
+    replica2.insert(0, TodoItem(5, "Cook dinner"))
 
     val mergedState = state.merge(replica1, replica2)
 
     assert(mergedState.size === 4)
-    assert(mergedState.indexOf(cleanHouse) == Some(0))
-    assert(mergedState.indexOf(cookDinner) == Some(1))
-    assert(mergedState.indexOf(buyMilk) == Some(2))
-    assert(mergedState.indexOf(doLaundry) == Some(3))
+    assert(mergedState.indexOf(TodoItem(4, "Clean the house")) == Some(0))
+    assert(mergedState.indexOf(TodoItem(5, "Cook dinner")) == Some(1))
+    assert(mergedState.indexOf(TodoItem(1, "Buy milk")) == Some(2))
+    assert(mergedState.indexOf(TodoItem(3, "Do laundry")) == Some(3))
   }
 }
