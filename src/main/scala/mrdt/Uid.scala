132f9b46780ecb45ed76958d1f77df4419259267
Bennet Bo Fenner <bennet@zed.dev>
2024-05-16 11:55:33 +0200

    add Uid

--- src/main/scala/mrdt/Uid.scala
+++ src/main/scala/mrdt/Uid.scala
@@ -1,5 +1,18 @@
 package mrdt
 
-type Uid = Int
+import java.util.Base64
 
-object Uid {}
+val UID_SIZE = 64
+
+case class Uid(value: Array[Byte])
+
+object Uid {
+  private val random = new java.util.Random()
+
+  val zero = 0
+
+  def gen: Uid =
+    val randomBytes = new Array[Byte](UID_SIZE)
+    random.nextBytes(randomBytes)
+    Uid(randomBytes)
+}
